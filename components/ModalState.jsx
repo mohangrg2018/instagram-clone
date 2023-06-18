83e9c20ed7ab91f60a557ef9a85dd2dac179bac0
mohangrg <mohangrg2030@gmail.com>
2023-06-18 10:41:39 +0545

    upload modal component completed

--- components/ModalState.jsx
+++ components/ModalState.jsx
@@ -1,5 +1,5 @@
 import { modalState } from "@/atom/ModalAtom";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useRecoilState } from "recoil";
 import Modal from "react-modal";
 import { AiOutlineCamera } from "react-icons/ai";
@@ -8,20 +8,46 @@ const ModalState = () => {
   const [open, setOpen] = useRecoilState(modalState);
   const filePickerRef = useRef(null);
 
+  const [image, setImage] = useState(null);
+
+  const handleFilePickerChange = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+  };
+
   return (
     <div>
       {open && (
         <Modal
           isOpen={open}
-          onRequestClose={() => setOpen(false)}
-          className="absolute top-[30%] left-[50%] translate-x-[-50%] flex flex-col items-center gap-4 border border-gray-400 rounded-md p-5 bg-gray-100"
+          onRequestClose={() => {
+            setOpen(false);
+            setImage(null);
+          }}
+          className="absolute top-[100px] left-[50%] translate-x-[-50%] flex flex-col items-center gap-4 border border-gray-400 rounded-md p-5 bg-gray-100"
         >
-          <AiOutlineCamera
-            className="text-3xl cursor-pointer"
-            onClick={() => filePickerRef.current.click()}
+          {image ? (
+            <img src={image} alt="Uploaded Photo" />
+          ) : (
+            <AiOutlineCamera
+              className="text-3xl cursor-pointer"
+              onClick={() => filePickerRef.current.click()}
+            />
+          )}
+
+          <input
+            type="file"
+            className="hidden"
+            ref={filePickerRef}
+            onChange={handleFilePickerChange}
           />
-          <input type="file" className="hidden" ref={filePickerRef} />
-          <h2 className="font-medium">Upload a Photo</h2>
+          <h2 className="font-medium text-sm">Upload a Photo</h2>
           <input
             type="text"
             placeholder="Add a caption"
@@ -29,7 +55,7 @@ const ModalState = () => {
           />
           <button
             type="button"
-            className="text-sm bg-red-400 text-white p-3 rounded-md"
+            className="w-full py-2 text-sm bg-red-400 text-white rounded-md"
           >
             Upload Post
           </button>
